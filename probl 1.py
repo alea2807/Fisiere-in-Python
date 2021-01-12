bf4800ab0bf428c3fd4ef0fc88f94e9bdd5ef322
alexandra605 <70931822+alexandra605@users.noreply.github.com>
2021-01-12 22:25:25 +0200

    Update probl 1.py

--- probl 1.py
+++ probl 1.py
@@ -1,13 +1,10 @@
-with open ('numere.txt','r') as f:
+with open("C:/Users/PC/numere.txt",'r') as f:
     a=f.readline()
     b=f.readline()
 if int(a)>int(b):
-    x=int(a)*2
-    y=int(a)*3
-if int(b)>int(a):
-    x=int(b)*2
-    y=int(b)*3
-with open ('produs.txt','w') as f:
-    f.write(str(x))
-    f.write(str('   '))
-    f.write(str(y))
+    print(b)
+if int(a)<int(b):
+    print(a)
+with open('c:/Users/PC/minim.txt','w') as f:   
+    f.write(str(a))or(str(b))
+    
